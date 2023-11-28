3babf0cd337b963376ca7f49bcaadbe763c13194
MNOJC <nathan.moulin718@gmail.com>
2023-11-28 23:12:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OverwatchArenaCpp
+
+Developed with Unreal Engine 5
---
+++ Source/OverwatchArenaCpp/OverwatchArenaCppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OverwatchArenaCppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERWATCHARENACPP_API AOverwatchArenaCppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
